e983e2e3a002736ffa22e194e3af20976bf0cfb1
Johan Sebastian Larrota Leal <sebitaslarrota@gmail.com>
2025-07-07 12:42:50 -0500

    Agrego el inicio de sesion de la red

---
+++ Inicio-de-sesion.py
@@ -0,0 +1,23 @@
+# ================================================================
+# 🔐 ISSUE #2 - INICIO DE SESIÓN
+# ================================================================
+
+def iniciar_sesion(path: str):
+    datos = leerJson(path)
+    while True:
+        Usuario = Validar_Texto("Ingrese su usuario para ingresar a la cuenta: ")
+        espacio()
+        for dato in datos:
+            if dato["usuario"] == Usuario:
+                while True:
+                    contraseña = Verificacion_Contraseña("Ingrese su contraseña: ")
+                    if dato["contrasena"] == contraseña:
+                        print(f"BIENVENIDOS A NUESTRA RED SOCIAL PROGRAMADOR {Usuario}")
+                        return Usuario
+                    else:
+                        print("Usuario no existe")
+                break
+        else:
+            print("usuario no existe")
+
+# usuario_logueado = iniciar_sesion("usuarios.json")
